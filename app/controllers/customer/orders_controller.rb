63a2ed2d0c7732d64f4c50bc97c0d4796be210fd
mayumi <happyeveryday05200520@gmail.com>
2025-03-11 11:58:58 +0900

    order_detailsをデータベースに反映させる

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -21,23 +21,40 @@ class Customer::OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
-    @order.customer_id = current_customer.id
+    ActiveRecord::Base.transaction do
+      @order = current_customer.orders.new(order_params)
+      # @order.customer_id = current_customer.id
 
-    if @order.save
-      OrderMailer.complete(@order).deliver_now
-      current_customer.cart_items.destroy_all
-      redirect_to success_orders_path(@order), notice: "購入が完了しました"
-    else
-      # binding.irb
-      render :new
+      if @order.save
+        create_order_details(@order)
+        OrderMailer.complete(@order).deliver_now
+        current_customer.cart_items.destroy_all
+        redirect_to success_orders_path(@order), notice: "購入が完了しました"
+      else
+        # binding.irb
+        render :new
+      end
     end
+  rescue ActiveRecord::RecordInvalid
+    flash[:alert] = "購入に失敗しました"
+    render :new
   end
+
   def success; end
 
   private
 
   def order_params
     params.require(:order).permit(:name, :postal_code, :prefecture, :address1, :address2, :postage, :billing_amount, :status)
+  end
+
+  def create_order_details(order)
+    current_customer.cart_items.each do |cart_item|
+      order.order_details.create!(
+        product_id: cart_item.product_id,
+        quantity: cart_item.quantity,
+        price: cart_item.product.price
+      )
+    end
   end
 end
